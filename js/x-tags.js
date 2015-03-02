2f95c638da51d61ca9147d5c082d0eed2af80e2b
SirePi <yamailbox@gmail.com>
2015-03-02 16:34:10 +0100

    Delete js

--- js/x-tags.js
+++
@@ -1,8 +0,0 @@
-var xtags = {};
-var xGistProto = Object.create(HTMLElement.prototype);
-
-xGistProto.createdCallback = function() {
-  this.innerHTML = "<b>I'm in the element's Shadow DOM!</b>";
-};
-
-xtags.gist = document.registerElement('x-gistss', {prototype: xGistProto});
